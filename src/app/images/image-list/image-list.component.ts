71e0ad79c5345992549ab0591533e1025aba886d
Kristin Kim <kristinhajinkim@gmail.com>
2021-04-28 03:06:10 -0400

    extract data from firebase

--- src/app/images/image-list/image-list.component.ts
+++ src/app/images/image-list/image-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from 'src/app/shared/image.service';
 
 @Component({
   selector: 'app-image-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-list.component.css']
 })
 export class ImageListComponent implements OnInit {
+  imageList: any[] = [];
+  rowIndexArray: any[] = [];
 
-  constructor() { }
+  constructor(private service: ImageService) { }
 
   ngOnInit(): void {
+
+    // snapshotChanges() returns observers so we subscribe to them
+    this.service.imageDetailList.snapshotChanges().subscribe(
+      list =>{
+        this.imageList = list.map(item => {return item.payload.val();});
+        this.rowIndexArray = Array.from(Array(Math.ceil(this.imageList.length/3)).keys());
+      }
+    );
   }
+  
 
 }
